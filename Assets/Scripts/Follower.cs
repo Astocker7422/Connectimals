7acaa314ed6c9b94cf5bf3e1e3813f6f76264043
Astocker <Astocker@mail.bradley.edu>
2019-06-01 00:20:40 -0500

    Unity Setup Commit

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    public float distance;
+
+    //The player
+    public Player player;
+
+    private Rigidbody rigid;
+
+    private bool isFollowing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = transform.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isFollowing)
+        {
+            MoveToPlayer();
+        }
+    }
+
+    public void SetIsFollowing(bool newFollowing)
+    {
+        isFollowing = newFollowing;
+    }
+
+    public void MoveToPlayer()
+    {
+        //Look at target
+        Vector3 v3 = player.transform.position - transform.position;
+        v3.y = 0.0f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(v3), turnSpeed * Time.deltaTime);
+
+        //If the enemy is out of attack range,
+        if (Vector3.Distance(transform.position, player.transform.position) > distance)
+        {
+            //Move towards target
+            rigid.velocity = new Vector3(transform.forward.x * speed, rigid.velocity.y, transform.forward.z * speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Controls input and behaviors of the player object
+//Attached to the player object
+public class Player : MonoBehaviour
+{
+    //Player attributes
+    public float speed;
+
+    private Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //Take the movement input and apply movement speed
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
+
+        //Apply movement variables
+        Movement(horizontal, vertical);
+    }
+
+    //Applies movement to the player using the RigidBody
+    void Movement(float horizontal, float vertical)
+    {
+        rigid.velocity = new Vector3(horizontal, 0.0f, vertical) * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Follower"))
+        {
+            other.GetComponent<Follower>().SetIsFollowing(true);
+        }
+    }
+}
